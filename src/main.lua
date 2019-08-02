d8c4be5e57954661e9df4373e1a08ff4fa4a6164
Pedro Desktop <pedroavelar@gmail.com>
2019-08-03 03:11:09 +0900

    Resolução 1920x1080, barrinhas de CPU e GPU

--- src/main.lua
+++ src/main.lua
@@ -3,15 +3,52 @@ Hero   = require 'Hero'
 
 -- Load resources
 function love.load()
+  love.window.setFullscreen(true, "desktop")
+  desktopWidth, desktopHeight, flags = love.window.getMode( )
+  width, height = 1920, 1080
+
+  font = love.graphics.newFont(14)
+
   instance = Hero(0, 0)
 end
 
 -- Called continuously. dt = delta time
 function love.update(dt)
+  updateStart = love.timer.getTime()
   instance:update(dt)
+  updateResult = love.timer.getTime() - updateStart
 end
 
 -- All drawing comes here
 function love.draw()
+  drawStart = love.timer.getTime()
+  love.graphics.scale(desktopWidth/width, desktopHeight/height)
   instance:draw()
+  drawResult = love.timer.getTime() - drawStart
+
+  drawUpdateDrawBars()
+end
+
+function drawUpdateDrawBars()
+  dangerLine = 0.8
+  barHeight = 10
+  fontHeight = font:getHeight()
+  if (updateResult*1000>dangerLine) then
+    love.graphics.setColor(1, 1, 0)
+  else
+    love.graphics.setColor(1, 1, 0, 0.2)
+  end
+  love.graphics.rectangle("fill", 0, height-barHeight*2, width*updateResult*1000, barHeight)
+  if (drawResult*1000>dangerLine) then
+    love.graphics.setColor(1, 0, 0)
+  else
+    love.graphics.setColor(1, 0, 0, 0.2)
+  end
+  love.graphics.rectangle("fill", 0, height-barHeight, width*drawResult*1000, barHeight)
+
+  love.graphics.setColor(1, 1, 1, 0.2)
+  love.graphics.rectangle("fill", width*dangerLine, height-barHeight*2, 2, barHeight*2)
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print("Update: "..updateResult*1000 .."ms", 10, height-barHeight*2+barHeight/2-fontHeight/2)
+  love.graphics.print("Draw: "..updateResult*1000 .."ms", 10, height-barHeight+barHeight/2-fontHeight/2)
 end
